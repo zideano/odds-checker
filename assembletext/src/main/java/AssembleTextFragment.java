b5940e7e5d3f7e0ed430463a955d9bdea5727c8e
zideano <jermaineandrethomas@gmail.com>
2018-02-11 18:12:22 +0000

    update and changes.

--- assembletext/src/main/java/AssembleTextFragment.java
+++ assembletext/src/main/java/AssembleTextFragment.java
@@ -1,7 +1,10 @@
+import com.sun.istack.internal.NotNull;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,7 +27,13 @@ public class AssembleTextFragment {
         }
     }
 
-    private String reassemble(final String fragmentProblem) {
+    /**
+     * @param fragmentProblem is the text body that we will reassemble
+     * @return the assembled string
+     */
+    private String reassemble(final @NotNull String fragmentProblem) {
+        int maxOverlap = 0;
+        String other = "";
 
         List<String> text = new ArrayList<>(Arrays.asList(fragmentProblem.split(";")));
 
@@ -32,25 +41,43 @@ public class AssembleTextFragment {
             return "";
         }
 
-        int maxOverlap = 0;
-        String overlapping = "";
-
-        return checkForAssembledString(text, maxOverlap, overlapping);
+        return checkForAssembledString(text, maxOverlap, other);
     }
 
-    private String checkForAssembledString(List<String> text, int maxOverlap, String overlapping) {
+    /**
+     * @param text list of text fragments to be compared for overlap
+     * @param maxOverlap is the maximum overlap between the reference and other text
+     * @param other the other text fragment to compare to the reference string
+     * @return the assembled string
+     */
+    private String checkForAssembledString(@NotNull List<String> text, int maxOverlap, String other) {
 
+        /*
+            Get reference string and update the list
+         */
         String referenceString = text.get(0);
         text.remove(referenceString);
 
-        return iterateForStringOverlap(text, maxOverlap, overlapping, referenceString);
+        return iterateForStringOverlap(text, maxOverlap, other, referenceString);
     }
 
-    private String iterateForStringOverlap(List<String> text, int maxOverlap, String other, String referenceString) {
+    /**
+     * @param text list of text fragments to be compared for overlap
+     * @param maxOverlap is the maximum overlap between the reference and other text
+     * @param other the other text fragment to compare to the reference string
+     * @param referenceString the starting string to be compared
+     * @return the assembled string
+     */
+    private String iterateForStringOverlap(@NotNull List<String> text, int maxOverlap, String other, String referenceString) {
 
         while (text.size() > 0) {
-            for (final String fragment : text) {
+            for (String fragment : text) {
+                int overlap;
 
+                /*
+                    First check is the reference string is already composed of the
+                    other text fragments in both the forward and reverse directions
+                 */
                 if (referenceString.contains(fragment)) {
                     maxOverlap = fragment.length();
                     other = fragment;
@@ -60,7 +87,11 @@ public class AssembleTextFragment {
                     other = fragment;
                 }
 
-                int overlap = overlap(referenceString, fragment);
+                /*
+                    Check for overlap between the reference string and the text
+                    fragments in both the forward and reverse directions
+                 */
+                overlap = overlap(referenceString, fragment);
                 if (overlap > maxOverlap) {
                     maxOverlap = overlap;
                     other = fragment;
@@ -73,7 +104,11 @@ public class AssembleTextFragment {
                 }
 
             }
-            referenceString = recombine(referenceString, other, maxOverlap);
+
+            /*
+               Get reference string and update the list again
+            */
+            referenceString = concatenateString(referenceString, other, maxOverlap);
             text.remove(other);
             maxOverlap = 0;
             other = "";
@@ -82,7 +117,13 @@ public class AssembleTextFragment {
         return referenceString;
     }
 
-    private String recombine(String reference, String other, int overlap) {
+    /**
+     * @param reference starting string to be compared with
+     * @param other the other strings to be compared against reference string
+     * @param overlap find the maximum overlap between reference & other
+     * @return concatenated string: reference + other or just reference when no overlap
+     */
+    private String concatenateString(@NotNull String reference, @NotNull String other, int overlap) {
 
         boolean forwardMatch = reference.substring((reference.length() - overlap), reference.length())
                 .equals(other.substring(0, overlap));
@@ -100,10 +141,15 @@ public class AssembleTextFragment {
         }
     }
 
-    private int overlap(final String str1, final String str2) {
-        int maxOverlap = str2.length() - 1;
+    /**
+     * @param referenceString the starting string to be compared
+     * @param other the string to compare the reference string against
+     * @return the maximum overlap between the reference string and other
+     */
+    private int overlap(final @NotNull String referenceString, final @NotNull String other) {
+        int maxOverlap = other.length() - 1;
 
-        while (!str1.regionMatches(str1.length() - maxOverlap, str2, 0,
+        while (!referenceString.regionMatches(true,referenceString.length() - maxOverlap, other, 0,
                 maxOverlap)) {
             maxOverlap--;
         }
